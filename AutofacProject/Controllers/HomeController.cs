6bf9b39f5a06a6a5ff8c467da780d61112e9747f
s_astana <s_astana@mail.ru>
2020-01-14 21:19:57 +0600

    first commit

---
+++ AutofacProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AutofacProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutofacProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRepository repo;
+        public HomeController(IRepository r)
+        {
+            repo = r;
+        }
+        public ActionResult Index()
+        {
+            return View(repo.List());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AutofacProject/Util/AutofacConfig.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using AutofacProject.Interfaces;
+using AutofacProject.Interfaces.Release;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutofacProject.Util
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            // получаем экземпляр контейнера
+            var builder = new ContainerBuilder();
+
+            // регистрируем контроллер в текущей сборке
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // регистрируем споставление типов
+//          builder.RegisterType<JsonRepository>().As<IRepository>();
+            builder.RegisterType<SQLRepository>().As<IRepository>();
+
+            // создаем новый контейнер с теми зависимостями, которые определены выше
+            var container = builder.Build();
+
+            // установка сопоставителя зависимостей
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
